5f3add2333fa9e2473bcbe2dacb2f20edb451cf3
Marvin88 <marvin88@yandex.ru>
2016-11-23 07:34:25 +0300

    Add development.php

---
+++ development.php
@@ -0,0 +1,311 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Arial</title>        
+        <link rel="stylesheet" href="/css/bootstrap.css" />
+        <link rel="stylesheet" href="/template_styles.css" />
+        <link rel="stylesheet" href="/css/jquery.bxslider.css" />
+        <link rel="stylesheet" href="/css/jquery.mCustomScrollbar.css" />
+        <!--<link rel="stylesheet" media="screen and (max-width: 1280px)" href="/css/standart.css" />
+        <link rel="stylesheet" media="screen and (min-width: 1600px)" href="/css/big.css" />
+        --><!-- Optional theme -->
+       
+        
+        <!-- Latest compiled and minified JavaScript -->
+        <script type="text/javascript" src="js/jquery-1.11.0.min.js"></script>
+        <script type="text/javascript" src="js/functions.js"></script>
+        <script type="text/javascript" src="js/jquery.bxslider.js"></script>
+        <script type="text/javascript" src="js/jquery.mCustomScrollbar.min.js"></script>
+        
+    </head>
+    
+    <header id="m-header">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-5">
+                    <div class="row">
+                        <div class="col-lg-6 no-p-l">
+                            <div class="logo">
+                                АРИАЛ
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="header-1">
+                                широкоформатная печать
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+                
+                <div class="col-lg-4">
+                    <div class="row">
+                        <div class="col-lg-5">
+                            <img  src="/images/phone-icon.png"/>
+                            (812) 33 45 333
+                        </div>
+                        <div class="col-lg-7 no-p-r">
+                            <img src="/images/clock-icon.png"/>
+                            c 10:00 ДО 22:00
+                            <img  src="/images/mail-icon.png" style="padding-left: 17px;" />
+                        </div>
+                    </div>
+                    
+                </div>
+                <div class="col-lg-2">
+                    <div class="lk-btn">
+                        ЛИЧНЫЙ КАБИНЕТ
+                        <img src="/images/door.png"/>
+                    </div>
+                </div>
+                <div class="col-lg-1 text-right no-p-r">
+                   <a href="#">
+                    <img style="margin-top: -1px;" src="/images/burger.png" title="Меню" alt="Меню"/>
+                   </a>
+                </div>
+            </div>
+        </div>
+    </header>
+    <body>
+    <main>
+        <section id ="breadcrumbs">
+            <div class="container">
+                <div class="bread-item">главная</div>
+                <div class="bread-seporator"><img src="/images/bread-seporator.png"/></div>
+                <div class="bread-item">производство</div>
+            </div>
+        </section>
+        <section id="development">
+            <div class="container">
+                <h2>ПРОИЗВОДСТВО</h2>
+                <div class="row">
+                    <div class="develom-item">  
+                        <div class="develop-img">
+                            <img src="/images/dev-item-1.png"/>
+                            <div class="develop-name">
+                                DURST Rho
+                                P10 HS
+                            </div>
+                            
+                        </div>
+                        <div class="develop-item-descr">
+                            <div class="img">
+                                <img src="/images/dev-icon-3.png"/>
+                            </div>
+                            <div class="text">
+                                Промышленная печать
+                                рекламной графики 
+                                на тканях
+                            </div>
+                        </div>
+                    </div>
+                    <div class="develom-item">  
+                        <div class="develop-img">
+                            <img src="/images/dev-item-1.png"/>
+                            <div class="develop-name">
+                                DURST Rho
+                                P10 HS
+                            </div>
+                            
+                        </div>
+                        <div class="develop-item-descr">
+                            <div class="img">
+                                <img src="/images/dev-icon-5.png"/>
+                            </div>
+                            <div class="text">
+                                Промышленная печать
+                                рекламной графики 
+                                на тканях
+                            </div>
+                        </div>
+                    </div>
+                    <div class="develom-item">  
+                        <div class="develop-img">
+                            <img src="/images/dev-item-1.png"/>
+                            <div class="develop-name">
+                                DURST Rho
+                                P10 HS
+                            </div>
+                            
+                        </div>
+                        <div class="develop-item-descr">
+                            <div class="img">
+                                <img src="/images/dev-icon-3.png"/>
+                            </div>
+                            <div class="text">
+                                Промышленная печать
+                                рекламной графики 
+                                на тканях
+                            </div>
+                        </div>
+                    </div>
+                    <div class="develom-item">  
+                        <div class="develop-img">
+                            <img src="/images/dev-item-1.png"/>
+                            <div class="develop-name">
+                                DURST Rho
+                                P10 HS
+                            </div>
+                            
+                        </div>
+                        <div class="develop-item-descr">
+                            <div class="img">
+                                <img src="/images/dev-icon-3.png"/>
+                            </div>
+                            <div class="text">
+                                Промышленная печать
+                                рекламной графики 
+                                на тканях
+                            </div>
+                        </div>
+                    </div>
+                    <div class="develom-item">  
+                        <div class="develop-img">
+                            <img src="/images/dev-item-1.png"/>
+                            <div class="develop-name">
+                                DURST Rho
+                                P10 HS
+                            </div>
+                            
+                        </div>
+                        <div class="develop-item-descr">
+                            <div class="img">
+                                <img src="/images/dev-icon-5.png"/>
+                            </div>
+                            <div class="text">
+                                Промышленная печать
+                                рекламной графики 
+                                на тканях
+                            </div>
+                        </div>
+                    </div>
+                    <div class="develom-item">  
+                        <div class="develop-img">
+                            <img src="/images/dev-item-1.png"/>
+                            <div class="develop-name">
+                                DURST Rho
+                                P10 HS
+                            </div>
+                            
+                        </div>
+                        <div class="develop-item-descr">
+                            <div class="img">
+                                <img src="/images/dev-icon-3.png"/>
+                            </div>
+                            <div class="text">
+                                Промышленная печать
+                                рекламной графики 
+                                на тканях
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="nav-bottom">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3">
+                        <div class="main-menu-bottom">
+                            <ul>
+                                <li><a href="#">продукция</a></li>
+                                <li><a href="#">О КОМПАНИИ</a></li>
+                                <li><a href="#">ПРОИЗВОДСТВО</a></li>
+                                <li><a href="#">ПОРТФОЛИО</a></li>
+                                <li><a href="#">КОНТАКТЫ</a></li>
+                                <li><a href="#">ДОСТАВКА</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 no-p-l no-p-r">
+                        <div class="map-main">
+                            <div class="map-btn">
+                                <a href="#">
+                                    ПОСМОТРЕТЬ НА КАРТАХ
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="main-contacts-bottom">
+                            <div class="row">
+                                <div class="col-lg-12 c-f-block1">
+                                    <div>
+                                        <img  src="/images/phone-icon.png"/>
+                                    </div>
+                                    
+                                    (812) 33 45 333
+                                </div>
+                                <div class="col-lg-12 c-f-block2">
+                                    <div>
+                                        <img  src="/images/point.png"/>
+                                    </div>
+                                     
+                                    ул. Газовая, д.10,
+                                    САНКТ-ПЕТЕРБУРГ
+                                </div>
+                                <div class="col-lg-12 c-f-block3">
+                                    <div>
+                                        <img  src="/images/mail-icon.png"/>
+                                    </div>
+                                     info@arial.ru
+                                </div>
+                                <div class="col-lg-12 social-footer">
+                                    <a href="#">
+                                        <img src="images/fb-icon.png"/>
+                                    </a>
+                                    <a href="#">
+                                        <img src="images/vk-icon.png"/>
+                                    </a>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+    </main>
+    <footer>
+        <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-3">
+                        <button class="ask-question">
+                            задать вопрос
+                        </button>
+                    </div>
+                    <div class="col-lg-2 col-lg-offset-3"> 
+                        <div class="row">
+                            <div class="col-lg-7 no-p-l no-p-r">
+                                <span class="cart-footer-summ">ваш заказ:</span>
+                            </div>
+                            <div class="col-lg-4 no-p-l no-p-r">
+                                <div class="cart-summ-footer">
+                                    33
+                                </div>
+                            </div>                          
+                        </div>
+                    </div> 
+                    <div class="col-lg-1 no-p-l no-p-r">
+                        <div class="summ-footer">
+                            15 000 
+                        </div>
+                        <span class="curency-footer">
+                            руб.
+                        </span>
+                    </div>
+                    <div class="col-lg-3 text-right">
+                        <button class="make-order">
+                            <img src="/images/cart.png" />
+                            оформить
+                        </button>
+                    </div>
+                </div>
+        </div>
+    </footer>
+    </body>
+</html>
+    
